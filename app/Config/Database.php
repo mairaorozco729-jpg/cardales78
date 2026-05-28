047bed416d9c9f7373105797a38a2525fbe10bfe
poscia_4 <mairaorozco729@gmail.com>
2026-05-28 15:22:05 -0500

    bas

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => 'yamanote.proxy.rlwy.net',
         'username'     => 'root',
-        'password'     => '',
-        'database'     => 'pos_db',
+        'password'     => 'qCbnjwBzENgoqEsfkmESNUlSfpQvgCsO',
+        'database'     => 'railway',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => 45723,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
